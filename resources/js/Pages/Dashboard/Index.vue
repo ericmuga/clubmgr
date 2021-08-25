ccf5a118e70907b57d12de04eb952b2ebd5862e9
Eric Muga <eric.muga@gmail.com>
2021-08-25 04:40:14 +0300

    initial commit

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
+   
+     <div class="flex">
+       <div class="flex-1">
+          The Dashboard content will come here
+       </div>
+     </div>
+
+  </div>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  metaInfo: { title: 'Dashboard' },
+  layout: Layout,
+}
+</script>
---
+++ resources/js/Pages/Members/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="mb-8 font-bold text-3xl">Members</h1>
+   
+     <div class="flex">
+       <div class="flex-1">
+          The Members content will come here
+     </div>
+ </div>
+
+  </div>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  metaInfo: { title: 'Members' },
+  layout: Layout,
+}
+</script>
